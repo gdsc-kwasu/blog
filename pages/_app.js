aaec5bab1ff4391c38974d3b66e3873e00dce30d
Caleb <belac335@gmail.com>
2021-09-16 07:24:56 +0100

    add: peer deps and local fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "/fonts/GoogleSans-Bold-v1.27.ttf";
+import "/fonts/GoogleSansDisplay-Regular-v1.27.ttf";
+import "/fonts/GoogleSans-Medium-v1.27.ttf";
+import "/fonts/GoogleSans-Regular-v1.27.ttf";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
