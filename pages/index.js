454e85a3eab20ad3025d1fff937daec1353d5451
Caleb <belac335@gmail.com>
2021-10-03 18:51:54 +0100

    fix: rename posts to articles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import fs from "fs";
-import path from "path";
 import Head from "next/head";
 import React from "react";
+import fs from "fs";
+import path from "path";
+// using the graymatter module to parse
+// the content of each article
+import matter from "gray-matter";
 
 export default function Home({ posts }) {
   console.log(posts);
@@ -19,19 +22,35 @@ export default function Home({ posts }) {
 
 export async function getStaticProps() {
   // obtain blog posts from the "/posts" directory
-  const files = fs.readdirSync(path.join("posts"));
+  const files = fs.readdirSync(path.join("articles"));
 
   // getting the frontmatter/blog post excerpt from
   // the posts directory in each file
   const posts = files.map((filename) => {
     const slug = filename.replace(".md", "");
+    console.log(slug);
+
+    // obtaining the frontmatter
+    // it is the excerpt of tha article/blog post, that'll contain
+    // the title of the article, the author's name,
+    // the date it was published and so on.
+    const articlePreview = fs.readFileSync(
+      path.join("articles", filename),
+      "utf-8"
+    );
+
+    // destructuring graymatter by assigning 'frontmatter'
+    // to the 'data' key
+    const { data: frontmatter } = matter(articlePreview);
 
     return {
       slug,
+      frontmatter,
     };
   });
 
   console.log(posts);
+
   return {
     props: {
       posts: "GDSC Kwasu blog posts",
