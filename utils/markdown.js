61a1e17c5761ae453efa35351f1fe09fd960fb80
Abdulbassit Abdullahi Funsho <cs.kwasu2019@gmail.com>
2021-12-09 19:23:22 +0100

    update markdown utils

--- utils/markdown.js
+++ utils/markdown.js
@@ -1,13 +1,16 @@
 const { marked } = require('marked')
-const path = require('path')
 
 // a custom image renderer to fix image path, allowing it to be used relative to the articles folder
 const renderer = {
   image(href, _, text) {
-    const absPath = path.resolve(process.cwd(), 'aricles', href)
-    const relPath = path.relative(path.join(process.cwd(), 'public'), absPath)
+    // implementation that doesn't use the path module
+    const imagePath = href
+      .replace(/^.*?[\/\\]public[\/\\](.*)$/, '$1')
+      .split(/[\/\\]/)
+      .filter(Boolean)
+      .join('/')
 
-    return `<img src="/${relPath}" alt="${text}" />`
+    return `<img src="/${imagePath}" alt="${text}" />`
   },
 }
 
