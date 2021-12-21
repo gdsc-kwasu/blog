a5d173ab75a38e012bddf4d664c32bc9bd9a23f9
Abdulbassit Abdullahi Funsho <cs.kwasu2019@gmail.com>
2021-12-21 15:14:09 +0100

    Added next config opt in to React strict mode added node-loader for node-canvas native module

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  reactStrictMode: true,
+  webpack: ({ config }) => {
+    // loader for node-canvas node native module
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: 'node-loader',
+    })
+
+    return config
+  },
+}
