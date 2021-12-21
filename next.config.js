0b9ba5e09c7dfb250f0e855ec89a02857e2da323
Abdulbassit Abdullahi Funsho <cs.kwasu2019@gmail.com>
2021-12-21 17:41:54 +0100

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: 'node-loader',
+    })
+    return config
+  },
   images: {
     loader: 'custom',
   },
