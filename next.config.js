94d6c013cef87abdafb450d9da8dfb975a427580
Abdulbassit Abdullahi Funsho <cs.kwasu2019@gmail.com>
2021-12-21 16:26:05 +0100

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-
-  // webcpack config to include node-loader for native module becasue of node-canvas
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.node$/,
-      loader: 'node-loader',
-    })
-
-    return config
-  },
-  experimental: {
-    outputStandalone: true,
-  },
+  target: 'experimental-serverless-trace',
 }
